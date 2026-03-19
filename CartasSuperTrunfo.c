c3bd533b0013a1be0ea3584aec9c26a14afee653
Rafaei Inácio <rafaekipar@gmail.com>
2026-03-19 02:05:27 +0000

    CartasSuperTrunfo ininciante

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,39 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  int NumCarta1=0;
+  char Estado1 [2]="";
+  char Codigo1 [3]="";
+  char NomeCidade1 [15]="";
+  int Populacao1=0;
+  float Pib1=0;
+  int Numeropontosturisticos1=0;
+
+  int NumCarta2;
+  char Estado2 [15];
+  char Codigo2 [3];
+  char NomeCidade2 [15];
+  int Populacao2;
+  float Pib2;
+  int Numeropontosturisticos2;
+
 
   // Área para entrada de dados
+  printf("Digite o numero da Primeira Carta:");
+  scanf("%d",&NumCarta1);
+
+  printf("Digite o Primeiro Estado:");
+  scanf("%s", Estado1);
+
+   printf("Digite a Primeiro Codigo:");
+  scanf("%s",Codigo1);
+
+  printf("Digite a Primeira Cidade:");
+  scanf("%s",NomeCidade1);
 
   // Área para exibição dos dados da cidade
+printf("Carta: %d\nEstado: %s\nCodigo: %s\nNomecidade: %s\n", NumCarta1, Estado1,Codigo1,NomeCidade1);
+printf("Estado: %s ",Estado1);
 
 return 0;
 } 
